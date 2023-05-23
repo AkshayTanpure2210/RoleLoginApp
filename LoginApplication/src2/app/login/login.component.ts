5df49e8d0384d61aa25184d0a7d1f344a1654c74
ErAkshay2210 <116140237+ErAkshay2210@users.noreply.github.com>
2023-05-23 18:37:21 +0530

    loginApp

---
+++ LoginApplication/src2/app/login/login.component.ts
@@ -0,0 +1,33 @@
+
+import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+import { User } from '../user';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+ user : User ={
+  email : '',
+  password: ''
+ }
+ 
+ loggedIn:any;
+ role:any;
+ 
+ constructor (private svc: AuthService,private router:Router){}
+
+ logIn(){
+  this.svc.logIn(this.user).subscribe((response)=>{
+      console.log(response.token);
+      console.log(response);
+  })
+ }
+
+
+
+}

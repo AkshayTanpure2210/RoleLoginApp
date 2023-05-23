79bb9b201ece9aa1091481304899d4f83eec4a61
ErAkshay2210 <116140237+ErAkshay2210@users.noreply.github.com>
2023-05-23 19:33:16 +0530

    newfile

--- LoginApplication/src3/app/login/login.component.ts
+++ LoginApplication/src3/app/login/login.component.ts
@@ -1,10 +1,39 @@
+
 import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Router } from '@angular/router';
+import { User } from '../user';
 
 @Component({
-  selector: 'app-login',
+  selector: 'applogin',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
 
+ user : User ={
+  email : '',
+  password: ''
+ }
+ loggedIn:any;
+ role:any;
+ 
+ constructor (private svc: AuthService,private router:Router){}
+
+ logIn(){
+  this.svc.logIn(this.user).subscribe((response)=>{
+    localStorage.setItem('jwtToken',response.token);
+    console.log(response.token)
+    localStorage.setItem("jwt",response.token)
+    console.log(response);
+    const role=this.svc.getRoleFromToken();
+    console.log(role);
+  })
+ }
+
+
+
 }
+
+
+
